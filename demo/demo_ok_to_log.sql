5bf4b6fc6f7849d234f8717255881e166c288684
Martin D'Souza <martin.dsouza@gmail.com>
2013-07-26 11:42:20 -0600

    New demos and renamed old demo.sql to demo_set_level

---
+++ demo/demo_ok_to_log.sql
@@ -0,0 +1,17 @@
+declare
+  l_log boolean;
+  l_log_str varchar2(255);  
+begin
+  
+  l_log := logger.ok_to_log(logger.g_permanent); -- Just change this with the level you want to test
+  
+  if l_log then
+    l_log_str := 'TRUE';
+  else
+    l_log_str := 'FALSE';
+  end if;
+  
+  dbms_output.put_line('Ok to log? ' || l_log_str);
+
+end;
+/
