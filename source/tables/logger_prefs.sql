5d3cea1efd74371501c12dca37b02ecdb1097026
Martin D'Souza <martin.dsouza@gmail.com>
2015-03-10 08:38:45 -0600

    Fixed trigger issues and added constraint for pref_name

--- source/tables/logger_prefs.sql
+++ source/tables/logger_prefs.sql
@@ -33,18 +33,21 @@ create or replace trigger  biu_logger_prefs
   before insert or update on logger_prefs
   for each row
 begin
+  :new.pref_name := upper(:new.pref_name);
+
   if :new.pref_name = 'LEVEL' then
     :new.pref_value := upper(:new.pref_value);
   end if;
 
-  $if not $$currently_installing $then
+  $if $$currently_installing is null or not $$currently_installing $then
     -- Since logger.pks may not be installed when this trigger is compiled, need to move some code here
-    -- TODO mdsouza: test that this works
-    if upper(:new.pref_value) not in (g_off_name, g_permanent_name, g_error_name, g_warning_name, g_information_name, g_debug_name, g_timing_name) then
-      raise_application_error(-20000, '"LEVEL" must be one of the following values: ' || 
-        g_off_name || ', ' || g_permanent_name || ', ' || g_error_name || ', ' || 
-        g_warning_name || ', ' || g_information_name || ', ' || g_debug_name || ', ' || 
-        g_timing_name);     
+    if 1=1
+      and :new.pref_name = 'LEVEL'
+      and upper(:new.pref_value) not in (logger.g_off_name, logger.g_permanent_name, logger.g_error_name, logger.g_warning_name, logger.g_information_name, logger.g_debug_name, logger.g_timing_name, logger.g_sys_context_name, logger.g_apex_name) then
+      raise_application_error(-20000, '"LEVEL" must be one of the following values: ' ||
+        logger.g_off_name || ', ' || logger.g_permanent_name || ', ' || logger.g_error_name || ', ' ||
+        logger.g_warning_name || ', ' || logger.g_information_name || ', ' || logger.g_debug_name || ', ' ||
+        logger.g_timing_name || ', ' || logger.g_sys_context_name || ', ' || logger.g_apex_name);
     end if;
 
     -- this is because the logger package is not installed yet.  We enable it in logger_configure
@@ -66,6 +69,7 @@ using (
   select 'PREF_BY_CLIENT_ID_EXPIRE_HOURS' pref_name, '12' pref_value from dual union
   select 'INSTALL_SCHEMA' pref_name, sys_context('USERENV','CURRENT_SCHEMA') pref_value from dual union
   -- #46
+  -- TODO mdsouza: drop unsupported functions
   select 'PLUGIN_FN_LOG' pref_name, 'NONE' pref_value from dual union
   select 'PLUGIN_FN_ERROR' pref_name, 'NONE' pref_value from dual union
   select 'PLUGIN_FN_PERMANENT' pref_name, 'NONE' pref_value from dual union
@@ -85,3 +89,29 @@ when matched then
 when not matched then
   insert (p.pref_name,p.pref_value)
   values (d.pref_name,d.pref_value);
+
+
+-- Ensure that pref_name is upper
+declare
+  l_count pls_integer;
+  l_constraint_name user_constraints.constraint_name%type := 'LOGGER_PREFS_CK1';
+  l_sql varchar2(255);
+begin
+  select count(1)
+  into l_count
+  from user_constraints
+  where 1=1
+    and table_name = 'LOGGER_PREFS'
+    and constraint_name = l_constraint_name;
+
+  if l_count = 0 then
+    update logger_prefs
+    set pref_name = upper(pref_name);
+
+    l_sql := 'alter table logger_prefs add constraint ' || l_constraint_name || ' check (pref_name = upper(pref_name))';
+
+    execute immediate l_sql;
+  end if;
+
+end;
+/
