bd71f4e34e08ff2da33b2c7733cd5714c828a9c0
Martin D'Souza <martin.dsouza@gmail.com>
2015-03-11 18:25:25 -0600

    #46 Plugin demo

---
+++ demos/demo_plugin.sql
@@ -0,0 +1,27 @@
+set serveroutput on
+
+create or replace procedure log_test_plugin(
+  p_rec in logger.rec_logger_log)
+as
+  l_text logger_logs.text%type;
+begin
+  dbms_output.put_line('In Plugin');
+
+  logger.log_error('Wont call plugin since recursion / infinite loop would occur');
+end;
+/
+
+exec logger.set_level(p_level => logger.g_debug);
+
+update logger_prefs
+  set pref_value = 'log_test_plugin'
+  where pref_name = 'PLUGIN_FN_ERROR';
+
+exec logger_configure;
+
+
+declare
+begin
+  logger.log_error('test');
+end;
+/
