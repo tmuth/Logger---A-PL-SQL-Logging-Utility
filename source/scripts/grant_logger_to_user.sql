7d02a4a47d85323ca0bff3f60b94f2604ebf2b41
Martin D'Souza <martin.dsouza@gmail.com>
2015-05-19 09:06:41 -0600

    #108 Added some additional prefs to the grant script. Will be helpful for developers.

--- source/scripts/grant_logger_to_user.sql
+++ source/scripts/grant_logger_to_user.sql
@@ -8,9 +8,9 @@ define to_user = '&1' -- This is the user to grant the permissions to
 whenever sqlerror exit sql.sqlcode
 
 grant execute on logger to &to_user;
-grant select on logger_logs to &to_user;
+grant select, delete on logger_logs to &to_user;
 grant select on logger_logs_apex_items to &to_user;
-grant select on logger_prefs to &to_user;
+grant select, update on logger_prefs to &to_user;
 grant select on logger_prefs_by_client_id to &to_user;
 grant select on logger_logs_5_min to &to_user;
 grant select on logger_logs_60_min to &to_user;
